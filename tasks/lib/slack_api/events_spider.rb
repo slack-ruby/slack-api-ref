58ffde63cd6940c1b49aff50ad494e042692588b
dblock <dblock@dblock.org>
2016-08-07 13:28:50 -0400

    Updated from Slack docs, 2016/08/07.

--- tasks/lib/slack_api/events_spider.rb
+++ tasks/lib/slack_api/events_spider.rb
@@ -1,38 +1,43 @@
 module SlackApi
   class EventsSpider < Spidey::AbstractSpider
-    handle 'https://api.slack.com/events', :process_list
+    handle 'https://api.slack.com/events/api', :process_list
 
     def process_list(page, _default_data = {})
       page.search('.card tr').each do |event|
         tds = event.search('td')
-        next unless tds.count == 2
-        name = tds.first.text
-        desc = tds.last.text
+        next unless tds.count == 3
+        name, desc, required_scope = tds.map(&:text).map(&:strip)
         href = event.search('a[href^="/events"]').first
-        handle resolve_url(href[:href], page), :process_event, name: name, desc: desc
+        handle resolve_url(href[:href], page), :process_event, name: name, desc: desc, required_scope: required_scope
       end
     end
 
     def process_event(page, data = {})
-        json_hash = {
-          'name' => data[:name],
-          'desc' => data[:desc] + '.',
-          'long_desc' => page.search('.card p').map(&:text).join(" ").gsub("\n", " ")
-        }
+      long_desc = page.search('.card p, h4').map(&:text).join(" ").gsub("\n", " ")
+      long_desc.gsub! /Events API compatibility.*$/, ''
+      long_desc.gsub! /.*API Scope:\s*[\w:]*/, ''
+      long_desc.strip!
 
-        example = JSON.parse(page.search('pre code')
-            .text
-            .gsub('…', '')
-            .gsub('...', '')
-            .gsub("\n", " ")
-            .gsub(/\s+/, " ")
-            .gsub(', }', '}')
-            .gsub(', ]', ']')
-        ) rescue nil
+      json_hash = {
+        'name' => data[:name],
+        'desc' => data[:desc] + '.',
+        'long_desc' => long_desc,
+        'required_scope' => data[:required_scope]
+      }
 
-        json_hash['example'] = example if example
+      example = JSON.parse(page.search('pre code')
+          .text
+          .gsub('…', '')
+          .gsub('...', '')
+          .gsub("\n", " ")
+          .gsub(/\s+/, " ")
+          .gsub(', }', '}')
+          .gsub(', ]', ']')
+      ) rescue nil
 
-        record(file_name: 'events/' + data[:name] + '.json', json: JSON.pretty_generate(json_hash))
+      json_hash['example'] = example if example
+
+      record(file_name: 'events/' + data[:name] + '.json', json: JSON.pretty_generate(json_hash))
     end
   end
 end
