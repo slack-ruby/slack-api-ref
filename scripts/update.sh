df60f8697d96dc5025b61642beafb84cb7b16f83
Matthias Endler <matthias.endler@trivago.com>
2017-03-04 18:26:14 +0100

    Improve error handling

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-bundle exec rake api:update && \
-bundle exec rake api:validate && \
-git config --global user.name "Slack API Ref Buildbot" && \
-git config --global user.email "buildbot@slack-api-ref.com" && \
-git add . && \
-git commit -m "Automatic update at `date`" && \
+# Immediately exit on error
+set -e
+set -o pipefail
+
+bundle exec rake api:update
+bundle exec rake api:validate
+
+gh_token="${GH_TOKEN-}"
+
+if [ -z "$gh_token" ]
+then
+  echo "GH_TOKEN is not set. Cannot proceed." >&2
+  exit 1
+fi
+
+git config --global user.name "Slack API Ref Buildbot"
+git config --global user.email "buildbot@slack-api-ref.com"
+git add .
+git commit -m "Automatic update at `date`"
 git push origin master
