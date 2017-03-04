54940a7633304e7f2cf9c1625beb88acf2048a46
Matthias Endler <matthias.endler@trivago.com>
2017-03-04 17:30:28 +0100

    Add username and mail for build bot

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,8 @@
 
 bundle exec rake api:update && \
 bundle exec rake api:validate && \
+git config --global user.name "Slack API Ref Buildbot" && \
+git config --global user.email "buildbot@slack-api-ref.com" && \
 git add . && \
 git commit -m "Automatic update at `date`" && \
 git push origin master
