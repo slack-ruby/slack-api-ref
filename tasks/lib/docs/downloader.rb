ffb1f58c25148022e106c408349778e8a420ad14
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-09-06 18:03:03 -0400

    Split method and events downloaders.

--- tasks/lib/docs/downloader.rb
+++ tasks/lib/docs/downloader.rb
@@ -1,3 +1,6 @@
+require_relative 'methods_downloader'
+require_relative 'events_downloader'
+
 module SlackApi
   module Docs
     class Downloader
@@ -5,69 +8,19 @@ module SlackApi
         @target_path ||= File.expand_path('../../../docs.slack.dev', __dir__)
       end
 
-      def events_dir
-        File.join(target_path, 'events')
-      end
-
-      def events_path
-        File.join(events_dir, 'events.json')
-      end
-
-      def methods_dir
-        File.join(target_path, 'methods')
-      end
-
-      def methods_path
-        File.join(methods_dir, 'methods.json')
-      end
-
-      def events_url
-        'https://docs.slack.dev/reference/events.json'
-      end
-
-      def methods_url
-        'https://docs.slack.dev/reference/methods.json'
-      end
-
-      def method_url(method)
-        "https://docs.slack.dev/reference/methods/#{method}.json"
-      end
-
-      def method_target_path(method_name)
-        File.join(methods_dir, "#{method_name}.json")
-      end
-
       def download!
-        download_methods!
-        download_events!
+        methods_downloader.download!
+        events_downloader.download!
       end
 
-      def download_methods!
-        puts "#{methods_url} => #{methods_path}"
-        FileUtils.mkdir_p(methods_dir)
-        URI.open(methods_url) do |file|
-          json = JSON.parse(file.read)
-          File.write(methods_path, JSON.pretty_generate(json))
-          json.each do |method|
-            method_name = method['name']
-            method_url = method_url(method_name)
-            method_target_path = method_target_path(method_name)
-            puts "#{method_url} => #{method_target_path}"
-            URI.open(method_url) do |method_file|
-              method_json = JSON.parse(method_file.read)
-              File.write(method_target_path, JSON.pretty_generate(method_json))
-            end
-          end
-        end
+      private
+
+      def methods_downloader
+        @methods_downloader ||= MethodsDownloader.new(target_path)
       end
 
-      def download_events!
-        puts "#{events_url} => #{events_path}"
-        URI.open(events_url) do |file|
-          json = JSON.parse(file.read)
-          FileUtils.mkdir_p(events_dir)
-          File.write(events_path, JSON.pretty_generate(json))
-        end
+      def events_downloader
+        @events_downloader ||= EventsDownloader.new(target_path)
       end
     end
   end
