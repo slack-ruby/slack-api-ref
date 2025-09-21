326bd5cebbc413bc464b599978a6b6595a839260
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-09-21 11:54:33 -0400

    Update events incrementally.

---
+++ tasks/lib/slack_api/events_generator.rb
@@ -0,0 +1,44 @@
+module SlackApi
+  class EventsGenerator
+    def downloader
+      @downloader ||= SlackApi::Docs::Downloader.new
+    end
+
+    def generate!
+      events = JSON.load_file(downloader.events_downloader.events_path)
+
+      events.each do |data|
+        event_data = {
+          'name' => data['name'],
+          'desc' => "#{data['description']}.",
+          'required_scope' => massage_scope(data['APIs'].first)
+        }
+        process_event(event_data)
+      end
+
+      existing_events = Dir.glob('events/**/*.json').grep_v(%r{/_patches\b})
+      updated_events = events.map { |event| "events/#{event['name']}.json" }
+      (existing_events - updated_events).each do |removed_event|
+        puts "(delete) #{removed_event}"
+        File.delete(removed_event)
+      end
+    end
+
+    private
+
+    def massage_scope(scope)
+      case scope
+      when 'Events' then 'RTM'
+      else scope
+      end
+    end
+
+    def process_event(data)
+      filename = "events/#{data['name']}.json"
+      puts filename
+      existing_event_data = File.exist?(filename) ? JSON.load_file(filename) : {}
+      all_data = existing_event_data.merge(data)
+      File.write(filename, JSON.pretty_generate(all_data))
+    end
+  end
+end
--- tasks/update.rake
+++ tasks/update.rake
@@ -1,25 +1,15 @@
-require_relative 'lib/slack_api/methods_generator'
-
 namespace :api do
-  namespace :methods do
-    desc 'Update methods.'
-    task :update do
-      Rake::Task['api:methods:clean_files'].invoke('methods')
-      Rake::Task['api:methods:clean_files'].invoke('groups')
-      SlackApi::MethodsGenerator.new.generate!
-    end
+  desc 'Delete all generated files except undocumented ones.'
+  task :clean_files, :dirs do |_t, args|
+    raise 'Missing :dirs' unless args[:dirs]
 
-    desc 'Delete all generated files except undocumented ones.'
-    task :clean_files, :dirs do |_t, args|
-      raise 'Missing :dirs' unless args[:dirs]
-
-      # keep _undocumented and _patches
-      files = Dir["./{#{Array(args[:dirs]).join(',')}}/*"].grep_v(%r{/_\w*\b})
-      FileUtils.rm_rf files
-    end
+    # keep _undocumented and _patches
+    files = Dir["./{#{Array(args[:dirs]).join(',')}}/*"].grep_v(%r{/_\w*\b})
+    FileUtils.rm_rf files
   end
 
   task :update do
     Rake::Task['api:methods:update'].invoke
+    Rake::Task['api:events:update'].invoke
   end
 end
